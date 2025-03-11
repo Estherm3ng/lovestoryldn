2632533205bf48fac1a9ecf30099080e734ae175
Estherm3ng <92167183+Estherm3ng@users.noreply.github.com>
2025-03-11 23:40:32 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const nextEl = document.getElementById("next");
 
 const totalImages = 30; // Fixed number of images
 const angle = 360 / totalImages; // 12 degrees per image
-const translateZ = 400; // Keep the same depth as the old style
+const translateZ = 900; // Keep the same depth as the old style
 
 // Set the translateZ value in CSS
 document.documentElement.style.setProperty("--translateZ", `${translateZ}px`);
