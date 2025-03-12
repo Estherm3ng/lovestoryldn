d004783b7a65b6b957ae8a3e3774304e86b85e9d
Estherm3ng <esther.meng.19@ucl.ac.uk>
2025-03-12 20:27:28 +0000

    horizontal

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const imageContainerEl = document.querySelector(".image-container");
-    const prevEl = document.getElementById("prev");
-    const nextEl = document.getElementById("next");
+const imageContainerEl = document.querySelector(".image-container");
+const prevEl = document.getElementById("prev");
+const nextEl = document.getElementById("next");
+const totalImages = 20;
+const angle = 360 / totalImages; // 18° per image
 
-    const totalImages = 20;
-    const angle = 360 / totalImages; // 18° per image
+let x = 0;
+let timer;
 
-    let x = 0;
-    let timer;
-
-    prevEl.addEventListener("click", () => {
-        x += angle; // Rotate by 18°
-        updateGallery();
-        clearTimeout(timer);
-    });
-
-    nextEl.addEventListener("click", () => {
-        x -= angle; // Rotate by 18°
-        updateGallery();
-        clearTimeout(timer);
-    });
-
-    function updateGallery() {
-        imageContainerEl.style.transform = ` perspective(1500px) rotateY(${x}deg)`;
-        timer = setTimeout(() => {
-            x -= angle; // Auto-rotate every 3 seconds
-            updateGallery();
-        }, 3000);
-    }
-
-    updateGallery();
+prevEl.addEventListener("click", () => {
+   x += angle; 
+  updateGallery();
+  clearTimeout(timer);
 });
+
+nextEl.addEventListener("click", () => {
+    x -= angle;
+  updateGallery();
+  clearTimeout(timer);
+});
+
+function updateGallery() {
+  imageContainerEl.style.transform = `perspective(1500px) rotateY(${x}deg)`;
+  timer = setTimeout(() => {
+    x -= angle;
+    updateGallery();
+  }, 3000);
+}
+
